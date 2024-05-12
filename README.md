65cc7e1a034312c6b0fe025498b986323fba1535
andreigarcia <andreigarciacuadra@gmail.com>
2024-05-12 22:27:32 +0200

    todo

---
+++ README.md
@@ -0,0 +1,6 @@
+user:
+admin
+pwd
+1234
+
+url: http://0.0.0.0:8082/my/
---
+++ data/moodle/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '7jPXGrdjHDwWeuO3oRa08jt3PPK0TqOY0.0.0.0';
+$CFG->bootstraphash = 'ced9936eae71dc458b1b70eca6754bca';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
